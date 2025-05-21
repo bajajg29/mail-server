514b9dfa1a3b45a9540207ad092775ee178a0aed
Gagandeep bajaj <114282370+bajajg29@users.noreply.github.com>
2025-05-21 15:43:16 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD; 
 
 const Connection = () => {
-    const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-8j16moz-shard-00-00.zwycdvh.mongodb.net:27017,ac-8j16moz-shard-00-01.zwycdvh.mongodb.net:27017,ac-8j16moz-shard-00-02.zwycdvh.mongodb.net:27017/?ssl=true&replicaSet=atlas-usw39h-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.sygdzid.mongodb.net/`;
     try {
         mongoose.connect(DB_URI, { useNewUrlParser: true });
         mongoose.set('strictQuery', false);
